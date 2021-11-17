ec69be41bf3d129997d7d20495817e2767251de7
unknown <turtlecutillar@gmail.com>
2021-11-18 00:16:33 +0800

    goog

--- connect_and_launch.py
+++ connect_and_launch.py
@@ -41,7 +41,7 @@ options.add_argument("--disable-dev-shm-usage")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-gpu")
 
-driver = uc.Chrome(options=options,executable_path=os.environ.get("CHROMEDRIVER_PATH"))
+driver = webdriver.Chrome(options=options,executable_path=os.environ.get("CHROMEDRIVER_PATH"))
 
 
 async def start_server():
